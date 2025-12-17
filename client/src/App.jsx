b06811a942fe4aa607c687e2100883ff16aa636a
Rafael Hertling <Rafael.Hertling@pilatus-aircraft.com>
2025-12-17 13:14:33 +0100

    feat: added docker-compose.yaml

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 import Header from './components/Header'
 
@@ -6,9 +5,6 @@ export default function App() {
   return (
     <>
       <Header />
-       <div className="p-10 text-3xl font-bold text-red-600">
-      Tailwind test
-    </div>
     </>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,10 +4,11 @@ function Header(){
     return (
         <>  
 
-                {/* <img src="src\assets\icons8-elden-ring-96.png"/>
+                <img src="src\assets\icons8-elden-ring-96.png"/>
                 <h1>Elden Ring Tracker</h1>
-                <Button>Login</Button>
-                <Button>Sign In</Button> */}
+
+                <button>Login</button>
+                <button>Sign In</button>
         </>
     );
 }
